33b7ae3bddbb7b4ce10d9445b3e550de66ef41c8
Frytak <frytak2855@gmail.com>
2022-11-11 22:28:16 +0100

    CSS

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var table = document.getElementById('table');
-var img = new Image();
-const pixels = [];
+/** @format */
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let button = document.getElementById('copy');
+let table = document.getElementById('table');
+let img = new Image();
+let pixels = [];
+let text = document.getElementById('text');
+
+console.log(table);
 
 let downScale = 12;
 
-img.src = "./yeah.jpg";
+img.src = './cat.jpg';
 img.width = img.width / downScale;
 img.height = img.height / downScale;
 
 canvas.width = img.width;
 canvas.height = img.height;
 
-window.onload = function() {
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-    img = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(img)
+window.onload = function () {
+	ctx.drawImage(img, 0, 0, img.width, img.height);
+	// img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	// console.log(img);
 
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
-    for (let i = 0; i < canvas.height; i++) {
-        pixels.push({row: [], rowDiv: document.createElement('tr')});
-        for (let j = 0; j < canvas.width * 4; j += 4) {
-            pixels[i].row.push({
-                r: imageData[i * j],
-                g: imageData[i * (j+1)],
-                b: imageData[i * (j+2)],
-                t: imageData[i * (j+3)],
-                cell: document.createElement('td')
-            });
-        }
-    }
+	let rows = [];
+	for (let j = 0; j < canvas.width * 4 * canvas.height * canvas.width; j += 4) {
+		rows.push({
+			r: imageData[j],
+			g: imageData[j + 1],
+			b: imageData[j + 2],
+			t: imageData[j + 3]
+		});
+	}
 
-    for (let i = 0; i < canvas.height; i++) {
-        table.appendChild(pixels[i].rowDiv);
+	for (let i = 0; i < rows.length / canvas.width; i += canvas.width) {
+		pixels.push(rows.slice(i, i + canvas.width));
+	}
 
-        for (let j = 0; j < canvas.width; j++) {
-            //pixels[i].row[j].cell.appendChild(document.createTextNode(i + ' ' + j));
-            pixels[i].row[j].cell.style.backgroundColor = `rgb(${pixels[i].row[j].r}, ${pixels[i].row[j].g}, ${pixels[i].row[j].b})`;
-            pixels[i].rowDiv.appendChild(pixels[i].row[j].cell);
-        }
-    }
+	console.log(imageData, pixels);
 
-    console.log(pixels);
+	for (let i = 0; i < canvas.height; i++) {
+		let tr = document.createElement('tr');
+		table.appendChild(tr);
 
-
-}
+		for (let j = 0; j < canvas.width; j++) {
+			let td = document.createElement('td');
+			td.appendChild(document.createTextNode('⠀'));
+			td.style.backgroundColor = `rgb(${pixels[i][j].r}, ${pixels[i][j].g}, ${pixels[i][j].b})`;
+			tr.appendChild(td);
+		}
+	}
+};
