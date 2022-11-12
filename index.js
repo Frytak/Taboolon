0f2322587dfdaafc312b46c8406045fa5c4e8c59
Frytak <frytak2855@gmail.com>
2022-11-12 15:07:22 +0100

    Yeee

--- index.js
+++ index.js
@@ -2,30 +2,88 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let button = document.getElementById('copy');
-let table = document.getElementById('table');
-let img = new Image();
-let pixels = [];
-let text = document.getElementById('text');
-
-console.log(table);
-
+const inputImage = document.getElementById('inputImage');
+const inputImageLabel = document.getElementById('inputImageLabel');
+const reader = new FileReader();
+const image = new Image();
 let downScale = 12;
 
-img.src = './cat.jpg';
-img.width = img.width / downScale;
-img.height = img.height / downScale;
+// Loading image
+inputImage.addEventListener('change', (e) => {
+	let img = document.createElement('img');
 
-canvas.width = img.width;
-canvas.height = img.height;
+	reader.onloadend = function () {
+		image.src = reader.result;
+		img.src = image.src;
 
-window.onload = function () {
-	ctx.drawImage(img, 0, 0, img.width, img.height);
-	// img = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	// console.log(img);
+		// Show image that will be transformed
+		inputImageLabel.replaceChildren(img);
 
+		// Set image label for initial size
+		let inputImageInitSize = document.getElementById('inputImageInitSize');
+		let text = document.createTextNode(`Initial dimensions: ${image.width} x ${image.height}`);
+		inputImageInitSize.replaceChildren(text);
+
+		// Set image label for downscale size
+		let inputImageTransSize = document.getElementById('inputImageTransSize');
+		text = document.createTextNode(`After transformation: ${Math.floor(image.width / downScale)} x ${Math.floor(image.height / downScale)}`);
+		inputImageTransSize.replaceChildren(text);
+
+		canvas.width = image.width / downScale;
+		canvas.height = image.height / downScale;
+	};
+
+	// Read in the image file as a data URL
+	if (inputImage.files[0]) {
+		reader.readAsDataURL(inputImage.files[0]);
+	} else {
+		img.src = '';
+	}
+});
+
+const transform = document.getElementById('transform');
+const table = document.getElementById('table');
+const scaleControls = document.getElementById('scaleControls');
+
+// Changing the scale
+scaleControls.childNodes.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		// If the imege is loaded add the new size
+		if (!image.src) return;
+
+		downScale = e.target.innerText.slice(0, -1);
+		canvas.width = image.width / downScale;
+		canvas.height = image.height / downScale;
+
+		let inputImageTransSize = document.getElementById('inputImageTransSize');
+		text = document.createTextNode(`After transformation: ${Math.floor(image.width / downScale)} x ${Math.floor(image.height / downScale)}`);
+		inputImageTransSize.replaceChildren(text);
+
+		// Delete the class of selected and add it to the new one
+		if (button.classList.contains('selected')) return;
+
+		scaleControls.childNodes.forEach((button) => {
+			if (button.classList === undefined) return;
+			button.classList.remove('selected');
+		});
+		e.target.classList.add('selected');
+	});
+});
+
+let arrows = [document.getElementById('firstArrow'), document.getElementById('secondArrow'), document.getElementById('thirdArrow')];
+
+// Transforming the image
+transform.addEventListener('click', () => {
+	const pixels = [];
+
+	// Reset the table
+	table.innerHTML = '';
+	// Draw the image on the canvas
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+	// Get the new scaled image data
 	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
+	// Get all the rows
 	let rows = [];
 	for (let j = 0; j < canvas.width * 4 * canvas.height * canvas.width; j += 4) {
 		rows.push({
@@ -36,12 +94,12 @@ window.onload = function () {
 		});
 	}
 
+	// Asign rows to their height
 	for (let i = 0; i < rows.length / canvas.width; i += canvas.width) {
 		pixels.push(rows.slice(i, i + canvas.width));
 	}
 
-	console.log(imageData, pixels);
-
+	// Draw to the table
 	for (let i = 0; i < canvas.height; i++) {
 		let tr = document.createElement('tr');
 		table.appendChild(tr);
@@ -53,4 +111,4 @@ window.onload = function () {
 			tr.appendChild(td);
 		}
 	}
-};
+});
